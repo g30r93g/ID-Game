84e877a5119869a41f51403db1fe7c8049c2763c
George Nick Gorzynski <georgegorzynski@me.com>
2025-04-03 19:50:19 +0100

    Replace `process.env` with references to `@/env` in application

--- providers/ConvexClerkClientProvider.tsx
+++ providers/ConvexClerkClientProvider.tsx
@@ -4,8 +4,9 @@ import { useAuth } from '@clerk/nextjs'
 import {ConvexProviderWithClerk} from "convex/react-clerk";
 import {ConvexReactClient} from "convex/react";
 import { ReactNode } from "react";
+import {env} from "@/env";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClerkClientProvider({ children }: { children: ReactNode }) {
   return (
--- providers/Posthog.tsx
+++ providers/Posthog.tsx
@@ -6,9 +6,10 @@ import { usePostHog } from 'posthog-js/react'
 
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
+import {env} from "@/env";
 
 export function PostHogProviderWrapper({ children }: { children: React.ReactNode }) {
-  if (process.env.NODE_ENV !== 'production') {
+  if (env.NODE_ENV !== 'production') {
     // only enable posthog in prod
     return <>{children}</>
   }
@@ -18,9 +19,9 @@ export function PostHogProviderWrapper({ children }: { children: React.ReactNode
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST || 'https://eu.i.posthog.com',
-      ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: env.NEXT_PUBLIC_POSTHOG_API_HOST || 'https://eu.i.posthog.com',
+      ui_host: env.NEXT_PUBLIC_POSTHOG_UI_HOST,
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       capture_pageview: false // Disable automatic pageview capture, as we capture manually
     })
