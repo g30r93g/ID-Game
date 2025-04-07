69f02e3dff659647af3eb55f4ff4fd4bd65c856c
George Nick Gorzynski <georgegorzynski@me.com>
2025-04-07 19:47:36 +0100

    Add server-side analytic events

--- app/game/[code]/page.tsx
+++ app/game/[code]/page.tsx
@@ -3,8 +3,24 @@ import { api } from "@/convex/_generated/api";
 import {fetchMutation, fetchQuery, preloadQuery} from "convex/nextjs";
 import {redirect} from "next/navigation";
 import {getAuthToken} from "@/lib/auth";
+import PostHogClient from "@/lib/posthog";
+import {currentUser} from "@clerk/nextjs/server";
 
 export default async function GamePage({ params }: { params?: Promise<{ code: string }> }) {
+  // Get current user's ID
+  const [token, user] = await Promise.all([
+    getAuthToken(),
+    currentUser(),
+  ]);
+  if (!user) {
+    console.error("No user is found")
+    redirect('/game');
+  }
+  if (!token) {
+    console.error("No authentication token found")
+    redirect('/game');
+  }
+
   // get join code
   const joinCode = (await params)?.code;
 
@@ -17,18 +33,20 @@ export default async function GamePage({ params }: { params?: Promise<{ code: st
     redirect('/game')
   }
 
-  // Get current user's ID
-  const token = await getAuthToken();
-  if (!token) {
-    console.error("No JWT for user")
-    redirect('/game');
-  }
+  const { capture } = PostHogClient();
 
   // Ensure the current user is a player, otherwise join them
   const isUserPlayer = await fetchQuery(api.game.isUserPlayer, { joinCode }, { token });
   if (!isUserPlayer) {
     try {
-      // todo: add posthog capture
+      capture({
+        distinctId: user.id,
+        event: "game_join",
+        properties: {
+          joinCode
+        }
+      });
+
       await fetchMutation(api.game.joinGame, {joinCode}, {token})
     } catch {
       redirect('/game');
