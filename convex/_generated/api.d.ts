7586f9e3470447326db6a4b9a10df82bb4b5e00d
George Nick Gorzynski <georgegorzynski@me.com>
2026-07-09 21:03:35 +0100

    feat: register Better Auth HTTP routes

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,6 +10,7 @@
 
 import type * as auth from "../auth.js";
 import type * as game from "../game.js";
+import type * as http from "../http.js";
 
 import type {
   ApiFromModules,
@@ -20,6 +21,7 @@ import type {
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   game: typeof game;
+  http: typeof http;
 }>;
 
 /**
